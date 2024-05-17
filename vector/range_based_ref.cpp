58513cde8393bd2250b59832bc183270752fd382
tkd3904 <tkd3904@naver.com>
2024-05-17 09:27:33 +0900

    0517

---
+++ vector/range_based_ref.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+void print_vector(std::vector<T> &vec){
+    for(typename std::vector<T>::iterator itr = vec.begin(); itr != vec.end(); ++itr){
+        std::cout << *itr << std::endl;
+    }
+}
+
+template <typename T>
+void print_vector_range_based(std::vector<T> &vec){
+    for(const auto &elem : vec){
+        std::cout << elem << std::endl;
+    }
+}
+
+int main(){
+    std::vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    
+    std::cout << "print vector" << std::endl;
+    print_vector(vec);
+    std::cout << "print vector range based" << std::endl;
+    print_vector_range_based(vec);
+
+    return 0;
+}
